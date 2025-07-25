0a231e4e3ca98fd3d94506bfa1c7104429a2adb4
clayton <hello@claytonc.dev>
2025-07-25 16:47:42 -0700

    fix: validate repo warrants a version bump/release

--- pkg/release.go
+++ pkg/release.go
@@ -26,7 +26,12 @@ func Release() {
 	log.Printf("current version is %s\n", currentVersion)
 
 	changeType := parseSemanticReleaseChangeType(commitsSinceRelease)
+	if changeType == noop {
+		log.Fatalf("changes since last release are insufficient - cancelling release...")
+	}
+
 	newVersion := updateVersion(currentVersion, changeType)
+
 	if err := validateTag(repo, newVersion.String()); err != nil {
 		log.Fatalf("error validating new tag against repo: %v", err)
 	}
