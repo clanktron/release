33bf3d2cc89f870af93b787eea6821f2bb37d960
clayton <hello@claytonc.dev>
2025-07-27 14:18:19 -0700

    chore: add tests for version incrementing

---
+++ pkg/version_test.go
@@ -0,0 +1,28 @@
+package release
+
+import (
+	"testing"
+)
+
+func TestUpdateVersion(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    Version
+		changeType semverChangeType
+		expected Version
+	}{
+		{"Major", Version{1, 1, 1}, major, Version{2, 1, 1}},
+		{"Minor", Version{1, 1, 1}, minor, Version{1, 2, 1}},
+		{"Patch", Version{1, 1, 1}, patch, Version{1, 1, 2}},
+		{"Noop",  Version{1, 1, 1}, noop,  Version{1, 1, 1}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := updateVersion(tt.input, tt.changeType)
+			if result != tt.expected {
+				t.Errorf("Expected: %+v, Got: %+v", tt.expected, result)
+			}
+		})
+	}
+}
